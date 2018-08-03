930c17a5c7e55f91fb158dffe81771e596b32ca0
Ronan <ronan.16@hotmail.com>
2018-08-03 16:45:34 -0300

    Projeto Criado

---
+++ src/main/java/com/univale/spring/UnivaleSpringApplication.java
@@ -0,0 +1,12 @@
+package com.univale.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivaleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnivaleSpringApplication.class, args);
+	}
+}
