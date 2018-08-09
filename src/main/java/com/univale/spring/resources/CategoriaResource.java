6d7f828c25234b2a1a170dbd2b2f8bb899f08e2e
Ronan <ronan.16@hotmail.com>
2018-08-09 12:13:30 -0300

    Testando dominio Categoria

--- src/main/java/com/univale/spring/resources/CategoriaResource.java
+++ src/main/java/com/univale/spring/resources/CategoriaResource.java
@@ -1,16 +1,27 @@
 package com.univale.spring.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.univale.spring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1,"Informatica");
+		Categoria c2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;	
 	}
 
 }
